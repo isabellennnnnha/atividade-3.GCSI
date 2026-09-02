33bad900f733d6488a491661d6434be164a00e49
isabellennnnnha <isabelledasilvasantana.7@gmail.com>
2026-09-02 03:57:26 +0000

    new file:   .gitignore 	modified:   README.md 	new file:   index.html 	new file:   script.js 	new file:   style.css

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # atividade-3.GCSI
+
+Este repositório contém uma atividade desenvolvida durante o semestre atual do curso de Informática.
+
+## Descrição
+
+O projeto apresenta uma lista de produtos utilizando JavaScript.
+
+Os produtos são armazenados em um array de objetos e são exibidos dinamicamente na página HTML.
+
+## Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Funcionalidades
+
+- Armazenamento de produtos em um array
+- Utilização de objetos
+- Exibição dinâmica dos produtos
+- Interação através de botão
+
+## Autora
+
+Isabelle da Silva Santana
---
+++ script.js
@@ -0,0 +1,35 @@
+const produtos = [
+    {
+        nome: "Caneta",
+        preco: 3
+    },
+    {
+        nome: "Lápis",
+        preco: 2
+    },
+    {
+        nome: "Caderno",
+        preco: 15
+    },
+    {
+        nome: "Borracha",
+        preco: 4
+    }
+];
+
+function mostrarProdutos() {
+
+    const lista = document.getElementById("produtos");
+
+    lista.innerHTML = "";
+
+    produtos.forEach(function(produto) {
+
+        lista.innerHTML += `
+            <div class="produto">
+                <h3>${produto.nome}</h3>
+                <p>Preço: R$ ${produto.preco}</p>
+            </div>
+        `;
+    });
+}
